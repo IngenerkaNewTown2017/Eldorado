7373693302bef58ee83a478fcee325f52a99bfe9
Mikhail Abramov <beavisabra@yandex.ru>
2019-09-27 10:55:08 +0400

    Версия Devalve

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Сайтец
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bannerPictureBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BannerPictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.апнрапрToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оштлщToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.щшгротToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.лдодлтToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.шлоттщToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оружиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рабыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bannerPictureBox)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BannerPictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bannerPictureBox
+            // 
+            this.bannerPictureBox.Image = global::Сайтец.Properties.Resources.banner1;
+            this.bannerPictureBox.Location = new System.Drawing.Point(12, 146);
+            this.bannerPictureBox.Name = "bannerPictureBox";
+            this.bannerPictureBox.Size = new System.Drawing.Size(362, 145);
+            this.bannerPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bannerPictureBox.TabIndex = 0;
+            this.bannerPictureBox.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 13;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 8, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox6, 12, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 10, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox5, 11, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 4, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 554);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(638, 38);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(215, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 32);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TabStop = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(533, 3);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(100, 32);
+            this.textBox6.TabIndex = 0;
+            this.textBox6.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(109, 3);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 32);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(321, 3);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 32);
+            this.textBox4.TabIndex = 0;
+            this.textBox4.TabStop = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(427, 3);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 32);
+            this.textBox5.TabIndex = 0;
+            this.textBox5.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 32);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            // 
+            // BannerPictureBox2
+            // 
+            this.BannerPictureBox2.Location = new System.Drawing.Point(429, 146);
+            this.BannerPictureBox2.Name = "BannerPictureBox2";
+            this.BannerPictureBox2.Size = new System.Drawing.Size(221, 145);
+            this.BannerPictureBox2.TabIndex = 0;
+            this.BannerPictureBox2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.апнрапрToolStripMenuItem,
+            this.оштлщToolStripMenuItem,
+            this.щшгротToolStripMenuItem,
+            this.лдодлтToolStripMenuItem,
+            this.шлоттщToolStripMenuItem,
+            this.оружиеToolStripMenuItem,
+            this.рабыToolStripMenuItem,
+            this.очкиToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.menuStrip1.Size = new System.Drawing.Size(660, 23);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // апнрапрToolStripMenuItem
+            // 
+            this.апнрапрToolStripMenuItem.Name = "апнрапрToolStripMenuItem";
+            this.апнрапрToolStripMenuItem.Size = new System.Drawing.Size(48, 19);
+            this.апнрапрToolStripMenuItem.Text = "Рыба";
+            // 
+            // оштлщToolStripMenuItem
+            // 
+            this.оштлщToolStripMenuItem.Name = "оштлщToolStripMenuItem";
+            this.оштлщToolStripMenuItem.Size = new System.Drawing.Size(77, 19);
+            this.оштлщToolStripMenuItem.Text = "Животные";
+            // 
+            // щшгротToolStripMenuItem
+            // 
+            this.щшгротToolStripMenuItem.Name = "щшгротToolStripMenuItem";
+            this.щшгротToolStripMenuItem.Size = new System.Drawing.Size(100, 19);
+            this.щшгротToolStripMenuItem.Text = "Самое нужное";
+            // 
+            // лдодлтToolStripMenuItem
+            // 
+            this.лдодлтToolStripMenuItem.Name = "лдодлтToolStripMenuItem";
+            this.лдодлтToolStripMenuItem.Size = new System.Drawing.Size(66, 19);
+            this.лдодлтToolStripMenuItem.Text = "Скрепки";
+            // 
+            // шлоттщToolStripMenuItem
+            // 
+            this.шлоттщToolStripMenuItem.Name = "шлоттщToolStripMenuItem";
+            this.шлоттщToolStripMenuItem.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.шлоттщToolStripMenuItem.Size = new System.Drawing.Size(118, 19);
+            this.шлоттщToolStripMenuItem.Text = "Подводные лодки";
+            // 
+            // оружиеToolStripMenuItem
+            // 
+            this.оружиеToolStripMenuItem.Name = "оружиеToolStripMenuItem";
+            this.оружиеToolStripMenuItem.Size = new System.Drawing.Size(63, 19);
+            this.оружиеToolStripMenuItem.Text = "Оружие";
+            // 
+            // рабыToolStripMenuItem
+            // 
+            this.рабыToolStripMenuItem.Name = "рабыToolStripMenuItem";
+            this.рабыToolStripMenuItem.Size = new System.Drawing.Size(48, 19);
+            this.рабыToolStripMenuItem.Text = "Рабы";
+            // 
+            // очкиToolStripMenuItem
+            // 
+            this.очкиToolStripMenuItem.Name = "очкиToolStripMenuItem";
+            this.очкиToolStripMenuItem.Size = new System.Drawing.Size(48, 19);
+            this.очкиToolStripMenuItem.Text = "Очки";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(660, 53);
+            this.panel1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 576);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.BannerPictureBox2);
+            this.Controls.Add(this.bannerPictureBox);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "глав форм";
+            ((System.ComponentModel.ISupportInitialize)(this.bannerPictureBox)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BannerPictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bannerPictureBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.PictureBox BannerPictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem апнрапрToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оштлщToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem щшгротToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem лдодлтToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem шлоттщToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оружиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рабыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очкиToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
